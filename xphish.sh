300dae8d9f2ca282c143f1b602122d66541d34f1
Rahul Bhatt <72143946+rahul-bhatt43@users.noreply.github.com>
2022-04-19 16:08:03 +0530

    Update xphish.sh

--- xphish.sh
+++ xphish.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ##   Xphish 	: 	Automated Phishing Tool
 ##   Author 	: 	Rahul Bhatt
-##   Version 	: 	2.2
+##   Version 	: 	1
 ##   Github 	: 	https://github.com/rahul-bhatt43/
 ## If you Copy Then Give the credits :)
 ## Thanks to
@@ -136,7 +136,7 @@ banner() {
 		${ORANGE} >    < |  |_> >|   Y  \|  | \___ \ |   Y  \
 		${ORANGE}/__/\_ \|   __/ |___|  /|__|/____  >|___|  /
 		${ORANGE}      \/|__|         \/          \/      \/              
-		${ORANGE}						${RED}Version : 2.2
+		${ORANGE}						${RED}Version : 1
 
 		${GREEN}[${WHITE}-${GREEN}]${CYAN} Tool Created by rahul-bhatt43 (Rahul.Bhatt)${WHITE}
 	EOF
@@ -147,7 +147,7 @@ banner_small() {
 		${BLUE}
 		${BLUE}  ░█░█░█▀█░█░█░▀█▀░█▀▀░█░█
 		${BLUE}  ░▄▀▄░█▀▀░█▀█░░█░░▀▀█░█▀█
-		${BLUE}  ░▀░▀░▀░░░▀░▀░▀▀▀░▀▀▀░▀░▀${WHITE} 2.2
+		${BLUE}  ░▀░▀░▀░░░▀░▀░▀▀▀░▀▀▀░▀░▀${WHITE}Version : 1
 	EOF
 }
 ## Dependencies
@@ -560,17 +560,16 @@ main_menu() {
 	cat <<- EOF
 		${RED}[${WHITE}::${RED}]${ORANGE} Select An Attack For Your Victim ${RED}[${WHITE}::${RED}]${ORANGE}
 
-		${RED}[${WHITE}01${RED}]${ORANGE} Facebook      ${RED}[${WHITE}11${RED}]${ORANGE} Twitch       ${RED}[${WHITE}21${RED}]${ORANGE} DeviantArt
-		${RED}[${WHITE}02${RED}]${ORANGE} Instagram     ${RED}[${WHITE}12${RED}]${ORANGE} Pinterest    ${RED}[${WHITE}22${RED}]${ORANGE} Badoo
-		${RED}[${WHITE}03${RED}]${ORANGE} Google        ${RED}[${WHITE}13${RED}]${ORANGE} Snapchat     ${RED}[${WHITE}23${RED}]${ORANGE} Origin
-		${RED}[${WHITE}04${RED}]${ORANGE} Microsoft     ${RED}[${WHITE}14${RED}]${ORANGE} Linkedin     ${RED}[${WHITE}24${RED}]${ORANGE} DropBox	
-		${RED}[${WHITE}05${RED}]${ORANGE} Netflix       ${RED}[${WHITE}15${RED}]${ORANGE} Ebay         ${RED}[${WHITE}25${RED}]${ORANGE} Yahoo		
-		${RED}[${WHITE}06${RED}]${ORANGE} Paypal        ${RED}[${WHITE}16${RED}]${ORANGE} Quora        ${RED}[${WHITE}26${RED}]${ORANGE} Wordpress
-		${RED}[${WHITE}07${RED}]${ORANGE} Steam         ${RED}[${WHITE}17${RED}]${ORANGE} Protonmail   ${RED}[${WHITE}27${RED}]${ORANGE} Yandex			
-		${RED}[${WHITE}08${RED}]${ORANGE} Twitter       ${RED}[${WHITE}18${RED}]${ORANGE} Spotify      ${RED}[${WHITE}28${RED}]${ORANGE} StackoverFlow
-		${RED}[${WHITE}09${RED}]${ORANGE} Playstation   ${RED}[${WHITE}19${RED}]${ORANGE} Reddit       ${RED}[${WHITE}29${RED}]${ORANGE} Vk
-		${RED}[${WHITE}10${RED}]${ORANGE} Tiktok        ${RED}[${WHITE}20${RED}]${ORANGE} Adobe        ${RED}[${WHITE}30${RED}]${ORANGE} XBOX
-		${RED}[${WHITE}31${RED}]${ORANGE} Mediafire     ${RED}[${WHITE}32${RED}]${ORANGE} Gitlab       ${RED}[${WHITE}33${RED}]${ORANGE} Github
+		${RED}[${WHITE}01${RED}]${ORANGE} Facebook      ${RED}[${WHITE}14${RED}]${ORANGE} Linkedin
+		${RED}[${WHITE}02${RED}]${ORANGE} Instagram     ${RED}[${WHITE}26${RED}]${ORANGE} Wordpress
+		${RED}[${WHITE}03${RED}]${ORANGE} Google        
+		${RED}[${WHITE}04${RED}]${ORANGE} Microsoft     
+		${RED}[${WHITE}05${RED}]${ORANGE} Netflix       
+		${RED}[${WHITE}33${RED}]${ORANGE} Github        
+		${RED}[${WHITE}10${RED}]${ORANGE} Tiktok         
+		${RED}[${WHITE}08${RED}]${ORANGE} Twitter       
+		${RED}[${WHITE}13${RED}]${ORANGE} Snapchat   
+		${RED}[${WHITE}10${RED}]${ORANGE} Tiktok        
 		${RED}[${WHITE}34${RED}]${ORANGE} Discord
 
 		${RED}[${WHITE}99${RED}]${ORANGE} About         ${RED}[${WHITE}00${RED}]${ORANGE} Exit
